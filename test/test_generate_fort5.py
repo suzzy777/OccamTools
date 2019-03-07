7d1bb67422d60d458d6520abe19b73a19a561a03
Morten Ledum <mortele@student.matnat.uio.no>
2019-03-07 11:02:19 +0100

    Add comprehensive testing for generate_random_uniform

--- test/test_generate_fort5.py
+++ test/test_generate_fort5.py
@@ -6,24 +6,39 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 from generate_fort5 import generate_uniform_random  # noqa: E402
 
 
-def test_generate_random_uniform():
+def check_remove_file(file_name):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+        return True
+    else:
+        return False
+
+
+def test_generate_random_uniform_path():
     # Test that path names given result in a file called 'fort.5' created in
     # the given path.
-    path_name = os.path.join(os.path.dirname(__file__), '..')
-    path_file = os.path.join(os.path.join(path_name, 'fort.5'))
-    if os.path.exists(path_file):
-        os.remove(path_file)
-    generate_uniform_random(1, [1, 1, 1], path=path_name)
-    assert os.path.exists(path_file)
-    if os.path.exists(path_file):
-        os.remove(path_file)
+    path = os.path.join(os.path.dirname(__file__), '..')
+    path_file = os.path.join(os.path.join(path, 'fort.5'))
+    check_remove_file(path_file)
 
+    generate_uniform_random(1, [1, 1, 1], path=path)
+    assert check_remove_file(path_file)
+
+
+def test_generate_random_uniform_file():
     # Test that a specific file name given results in a file of that name, at
     # the specified path is created.
     file_name = 'fort.5'
     file_name = os.path.join(os.path.dirname(__file__), file_name)
-    if os.path.exists(file_name):
-        os.remove(file_name)
+    check_remove_file(file_name)
+
+    generate_uniform_random(2, [2, 3, 4], path=file_name)
+    assert check_remove_file(file_name)
+
+
+def test_generate_random_uniform_header():
+    file_name = os.path.join(os.path.dirname(__file__), 'header_test.5')
+    check_remove_file(file_name)
 
     n_particles = 10
     box = [2, 7, 11]
@@ -44,6 +59,22 @@ def test_generate_random_uniform():
             assert float(line[i]) == pytest.approx(box[i], abs=1e-15)
             assert float(line[3]) == pytest.approx(0.0, abs=1e-15)
 
+    assert check_remove_file(file_name)
+
+
+def test_generate_random_uniform_n_molecules():
+    file_name = os.path.join(os.path.dirname(__file__), 'molecules_test.5')
+    check_remove_file(file_name)
+
+    n_particles = 10
+    box = [2, 2, 2]
+    generate_uniform_random(n_particles, box, path=file_name)
+    assert os.path.exists(file_name)
+
+    with open(file_name, 'r') as in_file:
+        for _ in range(2):
+            line = in_file.readline()
+
         # Expected:
         # ---------------------------------------------------------------------
         # Number of particles:
@@ -56,6 +87,22 @@ def test_generate_random_uniform():
             assert line[i] == e
         line = in_file.readline().strip()
         assert int(line) == n_particles
+
+
+def test_generate_random_uniform_positions():
+    file_name = os.path.join(os.path.dirname(__file__), 'pos_test.5')
+    check_remove_file(file_name)
+
+    n_particles = 100
+    box = [9.29580019,
+           6.12879582,
+           3.98020502]
+    generate_uniform_random(n_particles, box, path=file_name)
+    assert os.path.exists(file_name)
+
+    with open(file_name, 'r') as in_file:
+        for _ in range(4):
+            line = in_file.readline()
 
         for i in range(1, n_particles+1):
             # Expected:
@@ -78,9 +125,12 @@ def test_generate_random_uniform():
             assert y <= box[1]
             assert z <= box[2]
 
-        if os.path.exists(file_name):
-            os.remove(file_name)
+    check_remove_file(file_name)
 
 
 if __name__ == '__main__':
-    test_generate_random_uniform()
+    test_generate_random_uniform_path()
+    test_generate_random_uniform_file()
+    test_generate_random_uniform_header()
+    test_generate_random_uniform_n_molecules()
+    test_generate_random_uniform_positions()
