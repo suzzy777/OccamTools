606092f47078aaabeee67a2251549818d9176dce
Morten Ledum <mortele@student.matnat.uio.no>
2019-03-07 17:24:26 +0100

    Add tests for _check_box in xyz_to_fort5

--- test/test_xyz_to_fort5.py
+++ test/test_xyz_to_fort5.py
@@ -5,7 +5,7 @@ import pytest
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 # from xyz_to_fort5 import (xyz_to_fort5, _check_box,
 #                           _parse_comment_line)  # noqa: E402
-from xyz_to_fort5 import _parse_comment_line  # noqa: E402
+from xyz_to_fort5 import _parse_comment_line, _check_box  # noqa: E402
 
 
 def test_xyz_to_occam_parse_comment_line():
@@ -28,3 +28,29 @@ def test_xyz_to_occam_parse_comment_line():
     for line in lines:
         box = _parse_comment_line(line)
         assert box == pytest.approx(expected, abs=1e-15)
+
+
+def test_check_box():
+    box = [5.29589528, 295.29859815, 21.95820590]
+    box_mod = [b for b in box]
+    box_mod[0] = box_mod[0] + 1e-3
+    caught_none = False
+    try:
+        _check_box(None, None)
+    except ValueError:
+        caught_none = True
+    assert caught_none
+
+    caught_diff = False
+    try:
+        _check_box(box, box_mod)
+    except ValueError:
+        caught_diff = True
+    assert caught_diff
+
+    caught_no = False
+    try:
+        _check_box(box, box)
+    except ValueError:
+        caught_no = True
+    assert not caught_no
