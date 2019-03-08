a43a64a28825e9826f96333d2ea0247eaa15aa0d
Morten Ledum <mortele@student.matnat.uio.no>
2019-03-08 15:49:31 +0100

    Add read_fort1 script with testing

---
+++ src/read_fort1.py
@@ -0,0 +1,93 @@
+
+def _parse_bool(string):
+    if string.lower() in ('yes', 'y', '1', 't', 'true'):
+        return True
+    elif string.lower() in ('no', 'n', '0', 'f', 'false'):
+        return False
+    else:
+        raise ValueError(f"String not recognized as a boolean, {string}")
+
+
+class Fort1:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        with open(self.file_name, 'r') as in_file:
+            self.file_contents = in_file.readlines()
+
+    def _parse_line(self, line, index):
+        if 'title' in line:
+            self.title = self.file_contents[index+1].strip()
+        elif 'atoms' in line:
+            self.n_particles = int(self.file_contents[index+1].strip())
+        elif 'cutoff' in line and 'nl' not in line:
+            self.cutoff = float(self.file_contents[index+1].strip())
+        elif 'nl_cutoff' in line:
+            self.nl_cutoff = float(self.file_contents[index+1].strip())
+        elif 'nl_size' in line:
+            self.nl_size = int(self.file_contents[index+1].strip())
+        elif 'time_step' in line:
+            self.dt = float(self.file_contents[index+1].strip())
+        elif 'number_of_steps' in line:
+            self.cutoff = int(self.file_contents[index+1].strip())
+        elif 'simulated_ensemble' in line:
+            self.ensemble = self.file_contents[index+1].strip()
+        elif 'angle_function' in line:
+            self.angle_function = int(self.file_contents[index+1].strip())
+        elif 'trj_print' in line:
+            self.trj_print = int(self.file_contents[index+1].strip())
+        elif 'out_print' in line:
+            self.out_print = int(self.file_contents[index+1].strip())
+        elif 'pbc_traj' in line:
+            self.pbc_traj = _parse_bool(self.file_contents[index+1].strip())
+        elif 'mean_field' in line:
+            self.mean_field = self.file_contents[index+1].strip()
+        elif 'num_config_acc' in line:
+            self.num_config_acc = int(self.file_contents[index+1].strip())
+        elif 'pot_calc_freq' in line:
+            self.pot_calc_freq = int(self.file_contents[index+1].strip())
+        elif 'temperature_coupl' in line:
+            self.temperature_coupl = float(self.file_contents[index+1].strip())
+        elif 'target_pressure' in line:
+            self.target_pressure = float(self.file_contents[index+1].strip())
+        elif 'pressure_coupling' in line:
+            self.pressure_coupling = float(self.file_contents[index+1].strip())
+        elif 'velocity_read' in line:
+            self.velocity_read = _parse_bool(
+                self.file_contents[index+1].strip()
+            )
+        elif 'target_temperature' in line:
+            self.target_temperature = float(
+                self.file_contents[index+1].split()[0]
+            )
+        elif 'collision_freq' in line:
+            self.collision_frequency = float(
+                self.file_contents[index+1].strip()
+            )
+        elif 'SCF_lattice_update' in line:
+            self.density_lattice_update = int(
+                self.file_contents[index+1].strip()
+            )
+        elif 'intra_nonbonded' in line:
+            self.intra_nonbonded = _parse_bool(
+                self.file_contents[index+1].strip()
+            )
+        elif 'adaptive' in line:
+            self.adaptive = True
+            next_line = self.file_contents[index+1].split()
+            self.adaptive_region_start = float(next_line[0])
+            self.adaptive_region_end = float(next_line[1])
+            self.adaptive_transition_length = float(next_line[2])
+        elif 'end' in line:
+            pass
+        else:
+            raise ValueError(f"fort.1 line not recognized, {line}")
+
+    def read_file(self, file_name=None):
+        if file_name is not None:
+            self.file_name = file_name
+            with open(self.file_name, 'r') as in_file:
+                self.file_contents = in_file.readlines()
+
+        for i, line in enumerate(self.file_contents):
+            if i % 2 == 0:
+                self._parse_line(line, i)
---
+++ test/test_read_fort1.py
@@ -0,0 +1,76 @@
+import os
+import sys
+import pytest
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+from read_fort1 import _parse_bool, Fort1  # noqa: E402
+
+
+def test_parse_bool():
+    true = ['YES', 'Y', '1', 'T', 'TRUE']
+    false = ['NO', 'N', '0', 'F', 'FALSE']
+    for string in true:
+        assert _parse_bool(string)
+    for string in false:
+        assert not _parse_bool(string)
+
+    caught = False
+    try:
+        _parse_bool('a')
+    except ValueError:
+        caught = True
+    assert caught
+
+
+def test_read_fort1_file():
+    file_name = os.path.join(os.path.dirname(__file__), '..', 'data',
+                             'example_fort.1')
+    file_name_2 = os.path.join(os.path.dirname(__file__), '..', 'data',
+                               'example_2_fort.1')
+    fort1 = Fort1(file_name)
+    fort1.read_file()
+
+    with open(file_name, 'r') as in_file:
+        for _ in range(2):
+            line = in_file.readline().strip()
+        assert line == fort1.title
+        assert line == 'example_fort.1'
+
+    fort1.read_file(file_name_2)
+    with open(file_name_2, 'r') as in_file:
+        for _ in range(2):
+            line = in_file.readline().strip()
+        assert line == fort1.title
+        assert line == 'example_2_fort.1'
+
+
+def test_read_fort1_invalid():
+    file_name = os.path.join(os.path.dirname(__file__), 'invalid_fort.1')
+    with open(file_name, 'w') as out_file:
+        out_file.write('title:\n')
+        out_file.write('invalid_fort.1\n')
+        out_file.write('invalid_key:\n')
+        out_file.write('here be dragons\n')
+
+    caught = False
+    try:
+        fort1 = Fort1(file_name)
+        fort1.read_file()
+    except ValueError:
+        caught = True
+    assert caught
+    os.remove(file_name)
+
+
+def test_read_fort1_file_contents():
+    file_name = os.path.join(os.path.dirname(__file__), '..', 'data',
+                             'example_fort.1')
+    fort1 = Fort1(file_name)
+    fort1.read_file()
+    assert fort1.n_particles == 2500
+    assert fort1.dt == pytest.approx(0.03, abs=1e-15)
+    assert fort1.adaptive_region_start == pytest.approx(50.0, abs=1e-15)
+
+    assert type(fort1.angle_function) == int
+    assert type(fort1.temperature_coupl) == float
+    assert type(fort1.velocity_read) == bool
