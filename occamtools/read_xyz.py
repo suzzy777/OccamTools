4c2a6bb494d0d68803df7a9d598cb3fcfc0974ae
Morten Ledum <mortele@student.matnat.uio.no>
2019-03-12 17:03:26 +0100

    Parse xyz comment line to look for time step, box in read_xyz

--- occamtools/read_xyz.py
+++ occamtools/read_xyz.py
@@ -1,5 +1,14 @@
 import numpy as np
-from occamtools.xyz_to_fort5 import _parse_comment_line
+
+
+def _are_floats(*args):
+    are_floats = True
+    try:
+        for f in args:
+            _ = float(f)
+    except TypeError:
+        are_floats = False
+    return are_floats
 
 
 class Xyz:
@@ -8,15 +17,29 @@ class Xyz:
 
     def _allocate_arrays(self):
         self.type = np.zeros(self.n_particles)
+        self.time = np.zeros(self.n_time_steps)
         self.x = np.zeros(shape=(self.n_time_steps, self.n_particles))
         self.y = np.zeros(shape=(self.n_time_steps, self.n_particles))
         self.z = np.zeros(shape=(self.n_time_steps, self.n_particles))
 
-    def _parse_comment(self, line):
+    def _parse_comment_first(self, line):
         sline = line.split()
+        recognized = True
+        self.box = [None, None, None]
         if len(sline) == 4:
-            line = ' '.join(sline[:-1])
-        return _parse_comment_line()
+            if not _are_floats(*tuple(sline)):
+                recognized = False
+            else:
+                self.time[0] = float(sline[0])
+                self.box = [float(sline[1]), float(sline[2]), float(sline[3])]
+        elif len(sline) == 3:
+            if not _are_floats(*tuple(sline)):
+                recognized = False
+            else:
+                self.box = [float(sline[0]), float(sline[1]), float(sline[2])]
+        else:
+            recognized = False
+        return recognized
 
     def _parse_types(self, in_file):
         in_file.seek(0)  # Reset position to the start of the file
@@ -32,7 +55,7 @@ class Xyz:
                 ind += 1
             self.type[i] = self.type_dict[t]
 
-    def read_file(self, file_name=None):
+    def read_file(self, file_name=None, save=True):
         if file_name is not None:
             self.file_name = file_name
 
@@ -43,12 +66,16 @@ class Xyz:
             self.n_particles = int(in_file.readline())
             self.n_time_steps = self.num_lines // self.n_particles
             self._allocate_arrays()
+            line = in_file.readline()
+            self.comment_format_known = self._parse_comment_first(line)
             self._parse_types(in_file)
 
             in_file.seek(0)
             for time_step in range(self.n_time_steps):
                 for _ in range(2):
                     line = in_file.readline()
+                if self.comment_format_known:
+                    self.time[time_step] = float(line.split()[0])
                 for i in range(self.n_particles):
                     line = in_file.readline().split()
                     self.x[time_step, i] = float(line[1])
--- test/test_read_xyz.py
+++ test/test_read_xyz.py
@@ -7,6 +7,12 @@ file_name = os.path.join(os.path.dirname(__file__), os.pardir, 'data',
                          'example_fort.8')
 
 
+def _read_default_file_name():
+    xyz = Xyz(file_name)
+    xyz.read_file()
+    return xyz
+
+
 def test_read_xyz_file():
     other_file = os.path.join(os.path.dirname(__file__), os.pardir, 'data',
                               'example_fort.5')
@@ -30,8 +36,7 @@ def test_read_xyz_file():
 
 
 def test_read_xyz_types():
-    xyz = Xyz(file_name)
-    xyz.read_file()
+    xyz = _read_default_file_name()
     assert 'Ar' in xyz.type_dict
     assert xyz.type_dict['Ar'] == 0
     assert np.squeeze(xyz.type).shape == (25,)
@@ -39,8 +44,7 @@ def test_read_xyz_types():
 
 
 def test_read_xyz_positions():
-    xyz = Xyz(file_name)
-    xyz.read_file(file_name)
+    xyz = _read_default_file_name()
     assert xyz.x[0, 6] == pytest.approx(6.7048148063, abs=1e-9)
     assert xyz.x[2, 18] == pytest.approx(7.9447518060, abs=1e-9)
     assert xyz.x[8, 15] == pytest.approx(6.5780688198, abs=1e-9)
@@ -52,3 +56,12 @@ def test_read_xyz_positions():
     assert xyz.z[0, 6] == pytest.approx(4.4030514961, abs=1e-9)
     assert xyz.z[2, 18] == pytest.approx(1.5187645491, abs=1e-9)
     assert xyz.z[8, 15] == pytest.approx(3.3215257040, abs=1e-9)
+
+
+def test_read_xyz_time_array():
+    xyz = _read_default_file_name()
+    assert np.squeeze(xyz.time).shape == (12,)
+    assert xyz.time[0] == pytest.approx(0, abs=1e-9)
+    assert xyz.time[1] == pytest.approx(0.03, abs=1e-9)
+    assert xyz.time[2] == pytest.approx(0.3, abs=1e-9)
+    assert xyz.time[11] == pytest.approx(3.0, abs=1e-9)
