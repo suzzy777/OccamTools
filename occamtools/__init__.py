a1aa4ce1077521d4ac6d5d2f9b349e72c65106b2
Morten Ledum <mortele@student.matnat.uio.no>
2019-04-04 16:36:13 +0200

    Update dunder all in __init__.py

--- occamtools/__init__.py
+++ occamtools/__init__.py
@@ -2,5 +2,10 @@ from .occam_data import OccamData
 from .read_fort1 import Fort1
 from .read_fort7 import Fort7
 from .read_xyz import Xyz
+from .generate_fort5 import generate_uniform_random, generate_fcc
+from .replace_in_fort1 import replace_in_fort1
+from .replace_in_fort3 import Fort3Replacement, replace_in_fort3
 
-__all__ = ['OccamData', 'Fort1', 'Fort7', 'Xyz']
+__all__ = ['OccamData', 'Fort1', 'Fort7', 'Xyz', 'generate_uniform_random',
+           'generate_fcc', 'replace_in_fort1', 'Fort3Replacement',
+           'replace_in_fort3']
