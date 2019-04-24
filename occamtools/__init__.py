babf64b39ed95a1aa2d0bb19480b6d4377b746f2
Morten Ledum <morten.ledum@gmail.com>
2019-04-24 18:36:18 +0200

    Add histogram function to dunder all in __init__.py

--- occamtools/__init__.py
+++ occamtools/__init__.py
@@ -5,7 +5,8 @@ from .read_xyz import Xyz
 from .generate_fort5 import generate_uniform_random, generate_fcc
 from .replace_in_fort1 import replace_in_fort1
 from .replace_in_fort3 import Fort3Replacement, replace_in_fort3
+from .histogram import histogram
 
 __all__ = ['OccamData', 'Fort1', 'Fort7', 'Xyz', 'generate_uniform_random',
            'generate_fcc', 'replace_in_fort1', 'Fort3Replacement',
-           'replace_in_fort3']
+           'replace_in_fort3', 'histogram']
