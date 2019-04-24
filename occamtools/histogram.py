07a44120d7b9140445597c9839d97e97cc6d4e4c
Morten Ledum <morten.ledum@gmail.com>
2019-04-24 15:57:41 +0200

    Add dimension specification to histogram function

--- occamtools/histogram.py
+++ occamtools/histogram.py
@@ -1,15 +1,49 @@
+import warnings
 import numpy as np
 from occamtools.occam_data import OccamData
 
 
+def _check_dimension(dimension):
+    if dimension is None:
+        return 0
+    if isinstance(dimension, str):
+        if 'x' in dimension:
+            return 0
+        elif 'y' in dimension:
+            return 1
+        elif 'z' in dimension:
+            return 2
+    elif isinstance(dimension, int):
+        if dimension >= 0 and dimension <= 2:
+            return dimension
+        else:
+            error_str = (f'Dimension keyword must be 0 <= dimension <= 2, not '
+                         f'{dimension}.')
+            raise ValueError(error_str)
+    error_str = (f'Dimension keyword must be of type string (value x/y/z), or '
+                 f'int (value 0/1/2), not {type(dimension)}.')
+    raise TypeError(error_str)
+
+
 def histogram(data, dimension=None, time_steps=None):
     if isinstance(data, OccamData):
-        d = data.x
+        dim = _check_dimension(dimension)
+        if dim == 0:
+            d = data.x
+        elif dim == 1:
+            d = data.y
+        else:
+            d = data.z
     elif isinstance(data, np.ndarray):
         d = data
+        if dimension is not None:
+            warn_str = (f'When input to histogram is of type np.ndarray, the '
+                        f'dimension keyword (dimension={dimension}) is '
+                        f'ignored.')
+            warnings.warn(warn_str)
     else:
         error_str = (f'Given data must be of type OccamData or np.ndarray, not'
                      f' {type(data)}.')
         raise TypeError(error_str)
 
-    return np.zeros(2)
+    return np.zeros(2), np.zeros(2)
--- test/test_histogram.py
+++ test/test_histogram.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pytest
 from occamtools.occam_data import OccamData
 from occamtools.histogram import histogram as occamhist
 
@@ -11,13 +12,39 @@ def test_histogram_input_type():
     occamdata = OccamData(fort1_file, silent=True)
     npdata = occamdata.x
     for d in (occamdata, npdata):
-        hist = occamhist(d)
+        hist, _ = occamhist(d)
         assert isinstance(hist, np.ndarray)
 
     for d in (1, 'not', 2.45):
         caught = False
         try:
-            hist = occamhist(d)
+            occamhist(d)
         except TypeError:
             caught = True
         assert caught is True
+
+
+def test_histogram_dimension(capsys):
+    data = OccamData(fort1_file, silent=True)
+    for dim in (0, 1, 2, 'x', 'y', 'z', None):
+        hist, _ = occamhist(data, dimension=dim)
+        assert isinstance(hist, np.ndarray)
+
+    for dim in (-1, 3, 4):
+        caught = False
+        try:
+            occamhist(data, dimension=dim)
+        except ValueError:
+            caught = True
+        assert caught is True
+
+    for dim in ('a', 'not', 'here be dragons'):
+        caught = False
+        try:
+            occamhist(data, dimension=dim)
+        except TypeError:
+            caught = True
+        assert caught is True
+
+    with pytest.warns(UserWarning, match='dimension keyword'):
+        occamhist(data.x, dimension=1)
