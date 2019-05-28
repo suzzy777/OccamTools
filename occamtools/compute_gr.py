c2d24e0718f8f87d74ec1c5ded7317e9484cfdd9
Morten Ledum <morten.ledum@gmail.com>
2019-05-28 11:05:58 +0200

    Add preliminary code for computing g(r)

--- occamtools/compute_gr.py
+++ occamtools/compute_gr.py
@@ -1,8 +1,61 @@
 import numpy as np
+from occamtools.replace_in_fort3 import _is_int
 from occamtools import OccamData
 
 
-def compute_gr(data):
+def _gr_kernel(x, y, z, bx, by, bz, n_atoms):
+    r = np.empty(int(n_atoms * (n_atoms - 1) / 2))
+    ind = 0
+    for i in range(n_atoms):
+        for j in range(i+1, n_atoms):
+            dx = x[j] - x[i]
+            if (dx > bx * 0.5):
+                dx = dx - bx
+            if (dx <= - bx * 0.5):
+                dx = dx + bx
+
+            dy = y[j] - y[i]
+            if (dy > by * 0.5):
+                dy = dy - by
+            if (dy <= - by * 0.5):
+                dy = dy + by
+
+            dz = z[j] - z[i]
+            if (dz > bz * 0.5):
+                dz = dz - bz
+            if (dz <= - bz * 0.5):
+                dz = dz + bz
+
+            r[ind] = np.sqrt(dx*dx + dy*dy + dz*dz)
+            ind += 1
+    return r
+
+
+def compute_gr(data, steps=None, **kwargs):
     if not isinstance(data, OccamData):
         error_str = (f'Given data must be of type OccamData, not {type(data)}')
         raise TypeError(error_str)
+
+    box = data.box
+    x, y, z = data.x, data.y, data.z
+    n_time_steps, n_atoms = x.shape
+
+    if steps is None:
+        steps = range(n_time_steps)
+    elif _is_int(steps):
+        steps = [steps]
+    else:
+        steps = range(steps[0], steps[1]+1)
+
+    for step in steps:
+        print(step)
+        r = _gr_kernel(x[step, :], y[step, :], z[step, :],
+                       box[0], box[1], box[2], n_atoms)
+        hist, bins = np.histogram(r, **kwargs)
+        density = n_atoms / (box[0] * box[1] * box[2])
+        bins_adj = bins + np.mean(np.diff(bins)) / 2
+        volume_bins = (4/3) * np.pi * (bins_adj[1:]**3 - bins_adj[:-1]**3)
+
+        print(bins, volume_bins)
+        hist = hist.astype(float) / (volume_bins * density)
+    return hist, bins
--- test/test_compute_gr.py
+++ test/test_compute_gr.py
@@ -1,5 +1,50 @@
+import os
 import pytest
+import numpy as np
+from occamtools.occam_data import OccamData
 from occamtools.compute_gr import compute_gr
 
+
+fort1_file_name = os.path.join(os.path.dirname(__file__), os.pardir, 'data',
+                               'fort.1')
+f_tmp = os.path.join(os.path.dirname(__file__), os.pardir, 'data',
+                     'fort.1')
+
 def test_compute_gr_type():
-    pass
+    data = OccamData(fort1_file_name, silent=True)
+    compute_gr(data)
+
+    for inp in [None, np.empty(shape=(5, 11, 3)), 1, 'not occamdata']:
+        with pytest.raises(TypeError, match='type OccamData'):
+            compute_gr(inp)
+
+
+def test_compute_gr_simple():
+    data = OccamData(fort1_file_name, silent=True)
+    d0, d1, dr = 0, 2.5, 0.5
+    bins_expected = np.array([0.25, 0.75, 1.25, 1.75, 2.25, 2.75]) - dr/2
+    gr, bins = compute_gr(data, steps=1, bins=bins_expected,
+                          range=(0, 2.5))
+    gr_expected = [0.0, 0.45472840883398674, 1.1727206333087026,
+                   0.8602969896859208, 1.0436389710943959, 0.9463266886545127]
+    bins_expected = np.array([0.25, 0.75, 1.25, 1.75, 2.25, 2.75]) - dr/2
+
+    gr_expected = list(gr_expected)
+    gr = list(gr)
+    for _ in range(5):
+        gr_expected.append(None)
+        gr.append(None)
+
+    print("")
+    for be, bb in zip(bins_expected, bins):
+        print(f"{be if be is not None else -1:<20f}"
+              f"{bb if bb is not None else -1:<20f}"
+              f"{abs(be - bb) if (be is not None and bb is not None) else -1:<20f}")
+    print("")
+    for ge, gg in zip(gr_expected, gr):
+        print(ge, gg)
+        print(f"{ge:<20f} {gg:<20f} {abs(ge - gg):<20f}")
+
+
+if __name__ == '__main__':
+    test_compute_gr_simple()
